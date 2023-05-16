666c2df1bb7afaecf84ac1d1296933ce6cf3d667
Julien <julien.esnau@gmail.com>
2023-05-16 13:31:20 +0200

    Troisieme étape done et ajout de schéma Joi

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -1,14 +1,24 @@
+const Joi = require('joi');
 const CartModel = require('../models/cartModel');
 const ProductModel = require('../models/productModel');
+
 exports.createCart = async (req, res) => {
+  const schema = Joi.object({
+    userId: Joi.number().required(),
+  });
+
+  const { error } = schema.validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
   try {
     await CartModel.createCart(req.body.userId);
     res.status(201).send('Cart created');
   } catch (err) {
-    console.error(err); // Ajoute cette ligne pour afficher l'erreur dans la console
+    console.error(err);
     res.status(500).send(err.message);
   }
 };
+
 exports.getCart = async (req, res) => {
   try {
     const cart = await CartModel.getCart(req.params.id);
@@ -17,11 +27,20 @@ exports.getCart = async (req, res) => {
     }
     res.status(200).json(cart);
   } catch (err) {
-    console.error(err); 
+    console.error(err);
     res.status(500).send('Server error');
   }
 };
+
 exports.addProduct = async (req, res) => {
+  const schema = Joi.object({
+    productId: Joi.number().required(),
+    quantity: Joi.number().min(1).required(),
+  });
+
+  const { error } = schema.validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
   try {
     const product = await ProductModel.getProduct(req.body.productId);
     if (!product) {
@@ -34,7 +53,7 @@ exports.addProduct = async (req, res) => {
     );
     res.status(201).send('Product added to cart');
   } catch (err) {
-    console.error(err); 
+    console.error(err);
     res.status(500).send(err.message);
   }
 };
@@ -44,12 +63,19 @@ exports.removeProduct = async (req, res) => {
     await CartModel.removeProduct(req.params.id, req.params.productId);
     res.status(200).send('Product removed from cart');
   } catch (err) {
-    console.error(err); 
+    console.error(err);
     res.status(500).send(err.message);
   }
 };
 
 exports.updateQuantity = async (req, res) => {
+  const schema = Joi.object({
+    quantity: Joi.number().min(1).required(),
+  });
+
+  const { error } = schema.validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
   try {
     await CartModel.updateQuantity(
       req.params.id,
@@ -58,7 +84,7 @@ exports.updateQuantity = async (req, res) => {
     );
     res.status(200).send('Product quantity updated');
   } catch (err) {
-    console.error(err); // Ajoute cette ligne pour afficher l'erreur dans la console
+    console.error(err);
     res.status(500).send(err.message);
   }
 };
@@ -68,7 +94,7 @@ exports.checkout = async (req, res) => {
     await CartModel.checkout(req.params.id);
     res.status(200).send('Checkout successful');
   } catch (err) {
-    console.error(err); // Ajoute cette ligne pour afficher l'erreur dans la console
+    console.error(err);
     res.status(500).send(err.message);
   }
 };
